a185b2472efb6a7b26dd3557cc836fc57bb556dd
wdthor <wdthor@proton.me>
2024-01-07 05:52:09 +0100

    simple component section complete * Add Task and its story * Add dependencies and fix accessibility issue

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,13 @@
 /** @type { import('@storybook/vue3-vite').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/components/**/*.stories.js'],
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-actions",
+    "@storybook/addon-a11y"
   ],
   framework: {
     name: "@storybook/vue3-vite",
